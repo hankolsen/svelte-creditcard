7ebcbc838339f8efb968588eab43f4e613bb14d2
hank <hank@hank.se>
2021-07-04 00:17:16 +0200

    Update cardnumberinput test

--- src/Form/CardNumberInput.test.ts
+++ src/Form/CardNumberInput.test.ts
@@ -1,12 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/svelte';
+import { render, screen } from '@testing-library/svelte';
+import userEvent from '@testing-library/user-event';
 import CardNumberInput from './CardNumberInput.svelte';
 
 describe('CardNumberInput test', () => {
   it('should render', () => {
-    const { container, getByLabelText } = render(CardNumberInput);
-    expect(getByLabelText('Card Number')).toBeDefined();
+    const { container } = render(CardNumberInput);
+    const cardNumberInput = screen.getByLabelText(/card Number/i) as HTMLInputElement;
+    userEvent.type(cardNumberInput, '123');
+    expect(cardNumberInput.value).toBe('123');
+    userEvent.type(cardNumberInput, 'abc');
+    expect(cardNumberInput.value).toBe('123');
+    userEvent.type(cardNumberInput, '456789012345678901234567890');
+    expect(cardNumberInput.value).toBe('1234567890123456');
+
     expect(container).toMatchSnapshot();
   });
 });
