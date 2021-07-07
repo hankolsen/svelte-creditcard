35b7571a8ee3465d5c4c6279f84a49d42c1be994
Hank <hank@hank.se>
2021-07-07 11:50:43 +0200

    Add github link

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import InlineSVG from 'svelte-inline-svg';
   import CreditCard from '@/CreditCard/CreditCard.svelte';
   import Form from '@/Form/Form.svelte';
 </script>
@@ -9,3 +10,15 @@
     <Form />
   </div>
 </div>
+<footer>
+  <a class="github__link"
+     target="_blank"
+     rel="noopener noreferrer"
+     href="https://github.com/hankolsen/svelte-creditcard"
+  >
+    <div class="github__link-content">
+      <InlineSVG class="github__link-logo" src="logos/github.svg"/>
+      <span class="github__link-text">GitHub</span>
+    </div>
+  </a>
+</footer>
